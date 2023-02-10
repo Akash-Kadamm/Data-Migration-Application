9110390f454ea5e4903b3f46348b2d1f5347d59c
akashkad <akashkad@cybage.com>
2023-02-10 15:17:08 +0530

    Data migration API

---
+++ Data-Migration-API/src/main/java/com/ioffice/DataMigrationAPI/DataMigrationApiApplication.java
@@ -0,0 +1,13 @@
+package com.ioffice.DataMigrationAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataMigrationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataMigrationApiApplication.class, args);
+	}
+
+}
---
+++ Data-Migration-API/src/test/java/com/ioffice/DataMigrationAPI/DataMigrationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ioffice.DataMigrationAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataMigrationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
